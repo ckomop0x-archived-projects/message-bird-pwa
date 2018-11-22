ed652b9166f6913ac38f863fc5233af11bee65a8
Pavel Klochkov <pklochkov@degiro.nl>
2018-11-22 16:16:46 +0100

    Broadcasting

--- server.ts
+++ server.ts
@@ -17,7 +17,23 @@ wss.on('connection', (ws: WebSocket) => {
 
         //log the received message and send it back to the client
         console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
+
+        const broadcastRegex = /^broadcast\:/;
+
+        if (broadcastRegex.test(message)) {
+            message = message.replace(broadcastRegex, '');
+
+            //send back the message to the other clients
+            wss.clients
+                .forEach(client => {
+                    if (client != ws) {
+                        client.send(`Hello, broadcast message -> ${message}`);
+                    }
+                });
+
+        } else {
+            ws.send(`Hello, you sent -> ${message}`);
+        }
     });
 
     //send immediatly a feedback to the incoming connection
