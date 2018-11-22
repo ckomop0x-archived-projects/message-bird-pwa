3e453a734026ba21569d1ab11d9907eeca65db6c
Pavel Klochkov <pklochkov@degiro.nl>
2018-11-22 16:48:45 +0100

    another ts version

--- dist/server/server.js
+++ dist/server/server.js
@@ -1,64 +1,25 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const http = require("http");
-const WebSocketServer = require("ws");
+'use strict';
+Object.defineProperty(exports, '__esModule', {value: true});
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
 const app = express();
-var port = process.env.PORT || 5000;
-app.use(express.static(__dirname + '/'));
+//initialize a simple http server
 const server = http.createServer(app);
-server.listen(port);
-console.log('http server listening on %d', port);
-var wss = new WebSocketServer.Server({ server: server });
-console.log('websocket server created on port ${server.address().port}');
-wss.on('connection', function (ws) {
-    var id = setInterval(function () {
-        ws.send(JSON.stringify(new Date()), function () {
-        });
-    }, 1000);
-    console.log('websocket connection open');
-    ws.on('close', function () {
-        console.log('websocket connection close');
-        clearInterval(id);
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({server});
+wss.on('connection', (ws) => {
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message) => {
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
     });
+    //send immediatly a feedback to the incoming connection
+    ws.send('Hi there, I am a WebSocket server');
 });
-// //initialize a simple http server
-// const server = http.createServer(app);
-//
-// //initialize the WebSocket server instance
-// const wss = new WebSocket.Server({ server });
-//
-// wss.on('connection', (ws: WebSocket) => {
-//
-//     //connection is up, let's add a simple simple event
-//     ws.on('message', (message: string) => {
-//
-//         //log the received message and send it back to the client
-//         console.log('received: %s', message);
-//
-//         const broadcastRegex = /^broadcast\:/;
-//
-//         if (broadcastRegex.test(message)) {
-//             message = message.replace(broadcastRegex, '');
-//
-//             //send back the message to the other clients
-//             wss.clients
-//                 .forEach(client => {
-//                     if (client != ws) {
-//                         client.send(`Hello, broadcast message -> ${message}`);
-//                     }
-//                 });
-//
-//         } else {
-//             ws.send(`Hello, you sent -> ${message}`);
-//         }
-//     });
-//
-//     //send immediatly a feedback to the incoming connection
-//     ws.send('Hi there, I am a WebSocket server');
-// });
 //start our server
-server.listen(8999, () => {
+server.listen(process.env.PORT || 8999, () => {
     // @ts-ignore
     console.log(`Server started on port ${server.address().port} :)`);
 });
--- server.ts
+++ server.ts
@@ -1,72 +1,47 @@
 import * as express from 'express';
 import * as http from 'http';
-import * as WebSocketServer from 'ws';
+import * as WebSocket from 'ws';
 
 const app = express();
-var port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + '/'))
+//initialize a simple http server
+const server = http.createServer(app);
 
-const server = http.createServer(app)
-server.listen(port)
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
 
-console.log('http server listening on %d', port)
+wss.on('connection', (ws: WebSocket) => {
 
-var wss = new WebSocketServer.Server({server: server})
-console.log('websocket server created on port ${server.address().port}')
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message: string) => {
 
-wss.on('connection', function (ws) {
-    var id = setInterval(function () {
-        ws.send(JSON.stringify(new Date()), function () {
-        })
-    }, 1000)
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
 
-    console.log('websocket connection open')
+        const broadcastRegex = /^broadcast\:/;
 
-    ws.on('close', function () {
-        console.log('websocket connection close')
-        clearInterval(id)
-    })
-})
+        if (broadcastRegex.test(message)) {
+            message = message.replace(broadcastRegex, '');
 
-// //initialize a simple http server
-// const server = http.createServer(app);
-//
-// //initialize the WebSocket server instance
-// const wss = new WebSocket.Server({ server });
-//
-// wss.on('connection', (ws: WebSocket) => {
-//
-//     //connection is up, let's add a simple simple event
-//     ws.on('message', (message: string) => {
-//
-//         //log the received message and send it back to the client
-//         console.log('received: %s', message);
-//
-//         const broadcastRegex = /^broadcast\:/;
-//
-//         if (broadcastRegex.test(message)) {
-//             message = message.replace(broadcastRegex, '');
-//
-//             //send back the message to the other clients
-//             wss.clients
-//                 .forEach(client => {
-//                     if (client != ws) {
-//                         client.send(`Hello, broadcast message -> ${message}`);
-//                     }
-//                 });
-//
-//         } else {
-//             ws.send(`Hello, you sent -> ${message}`);
-//         }
-//     });
-//
-//     //send immediatly a feedback to the incoming connection
-//     ws.send('Hi there, I am a WebSocket server');
-// });
+            //send back the message to the other clients
+            wss.clients
+                .forEach(client => {
+                    if (client != ws) {
+                        client.send(`Hello, broadcast message -> ${message}`);
+                    }
+                });
+
+        } else {
+            ws.send(`Hello, you sent -> ${message}`);
+        }
+    });
+
+    //send immediatly a feedback to the incoming connection
+    ws.send('Hi there, I am a WebSocket server');
+});
 
 //start our server
-server.listen(8999, () => {
+server.listen(process.env.PORT || 8999, () => {
     // @ts-ignore
     console.log(`Server started on port ${server.address().port} :)`);
 });
