740ffaf060c85179c492cc5f90d7152cb4d75fcd
Pavel Klochkov <pklochkov@degiro.nl>
2018-11-22 16:36:24 +0100

    Heroku version

--- server.ts
+++ server.ts
@@ -1,44 +1,69 @@
 import * as express from 'express';
 import * as http from 'http';
-import * as WebSocket from 'ws';
+import * as WebSocketServer from 'ws';
 
 const app = express();
+var port = process.env.PORT || 5000;
 
-//initialize a simple http server
-const server = http.createServer(app);
+app.use(express.static(__dirname + '/'))
 
-//initialize the WebSocket server instance
-const wss = new WebSocket.Server({ server });
+const server = http.createServer(app)
+server.listen(port)
 
-wss.on('connection', (ws: WebSocket) => {
+console.log('http server listening on %d', port)
 
-    //connection is up, let's add a simple simple event
-    ws.on('message', (message: string) => {
+var wss = new WebSocketServer.Server({server: server})
+console.log('websocket server created on port ${server.address().port}')
 
-        //log the received message and send it back to the client
-        console.log('received: %s', message);
+wss.on('connection', function (ws) {
+    var id = setInterval(function () {
+        ws.send(JSON.stringify(new Date()), function () {
+        })
+    }, 1000)
 
-        const broadcastRegex = /^broadcast\:/;
+    console.log('websocket connection open')
 
-        if (broadcastRegex.test(message)) {
-            message = message.replace(broadcastRegex, '');
+    ws.on('close', function () {
+        console.log('websocket connection close')
+        clearInterval(id)
+    })
+})
 
-            //send back the message to the other clients
-            wss.clients
-                .forEach(client => {
-                    if (client != ws) {
-                        client.send(`Hello, broadcast message -> ${message}`);
-                    }
-                });
-
-        } else {
-            ws.send(`Hello, you sent -> ${message}`);
-        }
-    });
-
-    //send immediatly a feedback to the incoming connection
-    ws.send('Hi there, I am a WebSocket server');
-});
+// //initialize a simple http server
+// const server = http.createServer(app);
+//
+// //initialize the WebSocket server instance
+// const wss = new WebSocket.Server({ server });
+//
+// wss.on('connection', (ws: WebSocket) => {
+//
+//     //connection is up, let's add a simple simple event
+//     ws.on('message', (message: string) => {
+//
+//         //log the received message and send it back to the client
+//         console.log('received: %s', message);
+//
+//         const broadcastRegex = /^broadcast\:/;
+//
+//         if (broadcastRegex.test(message)) {
+//             message = message.replace(broadcastRegex, '');
+//
+//             //send back the message to the other clients
+//             wss.clients
+//                 .forEach(client => {
+//                     if (client != ws) {
+//                         client.send(`Hello, broadcast message -> ${message}`);
+//                     }
+//                 });
+//
+//         } else {
+//             ws.send(`Hello, you sent -> ${message}`);
+//         }
+//     });
+//
+//     //send immediatly a feedback to the incoming connection
+//     ws.send('Hi there, I am a WebSocket server');
+// });
 
 //start our server
 server.listen(8999, () => {
