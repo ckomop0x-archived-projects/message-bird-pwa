f7ca4129b43f6fef6e2e0050793b3a1292024548
Pavel Klochkov <paul.klochkov@gmail.com>
2018-12-19 23:06:18 +0100

    tests fix with wondow mock

--- src/scripts/client/__tests__/index.spec.tsx
+++ src/scripts/client/__tests__/index.spec.tsx
@@ -2,7 +2,14 @@ import * as React from 'react';
 import {shallow} from 'enzyme';
 import App from '../components/App/index';
 
+declare var window: any;
+
 it('renders the heading', () => {
+    window.Notification = () => ({
+        onshow: jest.fn(),
+        onclick: jest.fn()
+    });
+
     const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
 });
